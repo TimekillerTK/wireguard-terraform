f7d0fe57374397e0dff4ecb5187f098c361e5744
TimekillerTK <erwartungen@protonmail.com>
2019-03-25 17:28:20 +0100

    test script for post_deploy.sh

--- files/post_deploy.sh
+++ files/post_deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 # Steps to take after deployment is completed (run on Wireguard client). Might not be needed
+
+touch .\output\test_success.txt
--- output.tf
+++ output.tf
@@ -1,3 +1,5 @@
+# For testing purposes - improper value was being returned from local_exec 
+# Delete later
 output "eip" {
   value = "${aws_eip.vpn_instance_eip.public_ip}"
 }
